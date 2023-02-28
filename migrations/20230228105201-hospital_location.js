852780a330eb38767155de66cc81bbc1060e2879
jennaaaaaaaaa <ggm4954@gmail.com>
2023-02-28 20:49:09 +0900

    [수정] migrate, models

---
+++ migrations/20230228105201-hospital_location.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn('hospitals', 'location', {
+        type: Sequelize.GEOMETRY('POINT'),
+      }),
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
