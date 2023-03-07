d1066857ec61e9b5d83ab6aa81565ad40425f39c
Joominseok <jumseok12@gmail.com>
2023-03-07 22:41:58 +0900

    [수정] category repo service 수정 / map.ejs 간략화

--- repositories/category.repository.js
+++ repositories/category.repository.js
@@ -30,8 +30,10 @@ class CategoryRepository {
                             include: [
                                 {
                                     model: this.hospitalModel,
+                                    paranoid: false,
                                     as: 'hospitals',
                                     where: {
+                                        deletedAt: { [Op.lt]: 1 },
                                         longitude: { [Op.between]: longitude },
                                         latitude: { [Op.between]: latitude },
                                     },
