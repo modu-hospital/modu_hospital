ac88e51ea66f5e9077986dfc022b604ee1f5ff44
Joominseok <jumseok12@gmail.com>
2023-03-21 16:49:39 +0900

    [추가] 지도페이지 추가

--- routes/ejs.routes.js
+++ routes/ejs.routes.js
@@ -35,7 +35,11 @@ router.get('/users/resetpassword/:params', (req, res) => {
 });
 
 router.get('/map/hospitals', (req, res) => {
-    res.render('map.ejs', { components: 'map' });
+    res.render('hospital.map.ejs');
+});
+
+router.get('/map/pharmacies', (req, res) => {
+    res.render('pharmacy.map.ejs');
 });
 
 //원장님의 공간
