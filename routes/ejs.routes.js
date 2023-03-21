1a555ad1812a0ead4f37a3a69d61d29c0eb428f5
reggias <reggias010@gmail.com>
2023-03-21 17:25:20 +0900

    [수정] 예약 캘린더 수정

--- routes/ejs.routes.js
+++ routes/ejs.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth.middleware');
 const CreateError = require('../lib/errors');
 
 // 메인페이지
@@ -8,73 +9,101 @@ router.get('/', (req, res) => {
 });
 
 //유저 메인페이지
-router.get('/users', (req, res) => {
-    res.render('index.ejs', { components: 'user' });
+router.get('/users', auth, (req, res) => {
+    if (res.locals.user.role === 'customer') {
+        return res.render('index.ejs', { components: 'user' });
+    }
 });
 
 // 서비스관리자 페이지
-router.get('/admin', (req, res) => {
-    res.render('index.ejs', { components: 'admin' });
+router.get('/admin', auth, (req, res) => {
+    if (res.locals.user.role === 'manager') {
+        return res.render('index.ejs', { components: 'admin' });
+    }
 });
 
 // 예약페이지
-router.get('/users/reservation', (req, res) => {
-    res.render('index.ejs', { components: 'reservation' });
+router.get('/users/reservation', auth, (req, res) => {
+    if (res.locals.user.role === 'customer') {
+        return res.render('index.ejs', { components: 'reservation' });
+    }
 });
+
 // 마이페이지
-router.get('/users/mypage', (req, res) => {
-    res.render('index.ejs', { components: 'mypage' });
+router.get('/users/mypage', auth, (req, res) => {
+    if (res.locals.user.role === 'customer') {
+        return res.render('index.ejs', { components: 'mypage' });
+    }
 });
+
 //비밀번호 찾기 (이메일 발송) 페이지
 router.get('/findmypassword', (req, res) => {
     res.render('index.ejs', { components: 'findmypassword' });
 });
 
 // 비밀번호 재설정 페이지
-router.get('/users/resetpassword/:params', (req, res) => {
-    res.render('index.ejs', { components: 'resetpassword' });
+router.get('/users/resetpassword/:params', auth, (req, res) => {
+    if (res.locals.user.role === 'customer' || 'partner') {
+        return res.render('index.ejs', { components: 'resetpassword' });
+    }
 });
 
-router.get('/map/hospitals', (req, res) => {
-    res.render('map.ejs', { components: 'map' });
+router.get('/map/hospitals', auth, (req, res) => {
+    if (res.locals.user.role === 'customer' || 'partner') {
+        return res.render('map.ejs', { components: 'map' });
+    }
 });
 
 //원장님의 공간
-router.get('/hospital', (req, res) => {
-    res.render('index.ejs', { components: 'hospital' });
+router.get('/hospital', auth, (req, res) => {
+    if (res.locals.user.role === 'partner') {
+        return res.render('index.ejs', { components: 'hospital' });
+    }
 });
 
 //병원정보 등록 페이지
-router.get('/register', (req, res) => {
-    res.render('index.ejs', { components: 'hospitalRegister' });
+router.get('/register', auth, (req, res) => {
+    if (res.locals.user.role === 'partner') {
+        return res.render('index.ejs', { components: 'hospitalRegister' });
+    }
 });
 
 //병원정보 수정 페이지
-router.get('/edit', (req, res) => {
-    res.render('index.ejs', { components: 'hospitalEdit' });
+router.get('/edit', auth, (req, res) => {
+    if (res.locals.user.role === 'partner') {
+        return res.render('index.ejs', { components: 'hospitalEdit' });
+    }
 });
 
 //의사정보 등록 페이지
-router.get('/doctorRegister', (req, res) => {
-    res.render('index.ejs', { components: 'doctorRegister' });
+router.get('/doctorRegister', auth, (req, res) => {
+    if (res.locals.user.role === 'partner') {
+        return res.render('index.ejs', { components: 'doctorRegister' });
+    }
 });
 
 //의사한명 페이지
-router.get('/doctorEdit', (req, res) => {
+router.get('/doctorEdit', auth, (req, res) => {
     const doctorId = req.query.doctorId;
-    res.render('index.ejs', { components: 'doctorEdit' });
+    if (res.locals.user.role === 'partner') {
+        return res.render('index.ejs', { components: 'doctorEdit' });
+    }
 });
 
 //병원상세페이지
-router.get('/hospitals/:hospitalId', (req, res) => {
+router.get('/hospitals/:hospitalId', auth, (req, res) => {
     const id = req.query.id;
-    res.render('index.ejs', { components: 'hospitaldetail' });
+    if (res.locals.user.role === 'customer') {
+        return res.render('index.ejs', { components: 'hospitaldetail' });
+    }
 });
 
 //의사 시간 추가
-router.get('/doctorTime', (req, res) => {
+router.get('/doctorTime', auth, (req, res) => {
     const doctorId = req.query.doctorId;
-    res.render('index.ejs', { components: 'doctorTime' });
+    if (res.locals.user.role === 'partner') {
+        return res.render('index.ejs', { components: 'doctorTime' });
+    }
 });
 
 //page expired
